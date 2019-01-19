92701a70556f78b08c2400e4a622ea973f5d02c6
stnDani <daniel.florentin.stoian@gmail.com>
2019-01-19 13:32:01 +0200

    Hibernate - StudentDemo Transaction

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/entity/Student.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/entity/Student.java
@@ -28,7 +28,7 @@ public class Student {
 	}
 
 	public Student(String firstName, String lastName, String email) {
-		this.firstName = firstName;
+		this.firstName = firstName; 
 		this.lastName = lastName;
 		this.email = email;
 	}
