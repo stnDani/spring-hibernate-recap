4e9a890ef7188b945af1f55401a36486d6c46406
stnDani <daniel.florentin.stoian@gmail.com>
2019-01-20 16:58:24 +0200

    Hibernate - refactored UpdateStudentDemo

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -8,31 +8,26 @@ import org.hibernate.cfg.Configuration;
 import com.luv2code.hibernate.demo.entity.Student;
 
 public class UpdateStudentDemo {
+	private static final Logger logger = Logger.getLogger(UpdateStudentDemo.class);
 
+	private static final SessionFactory factory = new Configuration()
+			.configure("hibernate.cfg.xml")
+			.addAnnotatedClass(Student.class)
+			.buildSessionFactory();
+	
 	public static void main(String[] args) {
 		
-		final Logger logger = Logger.getLogger(UpdateStudentDemo.class);
-
-		SessionFactory factory = new Configuration()
-				.configure("hibernate.cfg.xml")
-				.addAnnotatedClass(Student.class)
-				.buildSessionFactory();
 		
 		try {
+			updateStudentFirstNameAndID(1, "Scooby");
 			
-			int studentId = 1;
-			String firstName = "Scooby";
-			
-			updateStudentFirstName(logger, factory, studentId, firstName);
-			
-			String email = "foo@gmail.com";
-			bulkUpdateEmail(logger, factory, email);
+			bulkUpdateWithEmail("foo@gmail.com");
 		} finally {
 			factory.close();
 		}
 	}
 
-	private static void updateStudentFirstName(final Logger logger, SessionFactory factory, int studentId, String firstName) {
+	private static void updateStudentFirstNameAndID(int studentId, String firstName) {
 		Session session = factory.getCurrentSession();
 		session.beginTransaction();
 		
@@ -48,7 +43,7 @@ public class UpdateStudentDemo {
 		logger.info("Done!");
 	}
 
-	private static void bulkUpdateEmail(final Logger logger, SessionFactory factory, String email) {
+	private static void bulkUpdateWithEmail(String email) {
 		Session session = factory.getCurrentSession();
 		session.beginTransaction();
 		
