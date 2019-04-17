56f3845041ac3fb49ccaef369670590d97702f52
stnDani <daniel.florentin.stoian@gmail.com>
2019-04-17 21:36:03 +0300

    Deleting jackson-binding-project

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/config/MySpringMVCDispatcherServletInitializer.java
+++
@@ -1,4 +0,0 @@
-package com.luv2code.springdemo.config;
-
-public class MySpringMVCDispatcherServletInitializer {
-}
--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/DemoRestController.java
+++
@@ -1,4 +0,0 @@
-package com.luv2code.springdemo.config.com.luv2code.springdemo.rest;
-
-public class DemoRestController {
-}
